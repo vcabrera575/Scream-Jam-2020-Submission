11aa82f796ab48b3762a2b7ea4b4c335600bba47
vcabrera575 <vcabrera575@gmail.com>
2020-10-13 21:45:36 -0600

    Added door and knocking sound

---
+++ Scream Jam 2020/Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool hasBeenKnocked = false;
+    public float knockCooldown = 5f; // Time in seconds
+    public float objectDistance = 4f; // Set distance for how far away the player is from interactable object
+    public float volume = 1f;
+
+    public Camera playerCamera;
+    public AudioSource doorSoundSource;
+    public AudioClip doorSound;
+
+
+    // Start is called before the first frame update
+    void Interact()
+    {
+        Vector3 origin = playerCamera.transform.position;
+        Vector3 direction = playerCamera.transform.forward;
+        RaycastHit hit;
+
+        if (Physics.Raycast(origin, direction, out hit, objectDistance))
+        {
+            hasBeenKnocked = true;
+            doorSoundSource.PlayOneShot(doorSound, volume);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Interact();
+        }
+    }
+}
