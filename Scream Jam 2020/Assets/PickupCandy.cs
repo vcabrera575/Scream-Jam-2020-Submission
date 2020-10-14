7cfc0cf17ff334ec75cab81630eabd902e2ad966
Famichad <72538165+Famichad@users.noreply.github.com>
2020-10-13 22:28:03 -0700

    added Candy object

---
+++ Scream Jam 2020/Assets/PickupCandy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCandy : MonoBehaviour
+{
+    public int value = 3;
+    void OnTriggerEnter(Collider collisionInfo)
+    {
+        if (collisionInfo.tag == "Player")
+        {
+            //add candy
+            GameObject.Find("GameController").GetComponent<GameController>().GetCandy(value);
+            Destroy(gameObject);
+        }
+    }
+}
--- Scream Jam 2020/Assets/Scripts/Door.cs
+++ Scream Jam 2020/Assets/Scripts/Door.cs
@@ -7,6 +7,7 @@ public class Door : MonoBehaviour
 {
     public bool hasBeenKnocked = false;
     public float knockCooldown = 5f; // Time in seconds
+    float knockTimer = 0f;
     public float objectDistance = 2f; // Set distance for how far away the player is from interactable object
     public float volume = 1f;
 
@@ -24,20 +25,24 @@ public class Door : MonoBehaviour
 
     void Update()
     {
-
+        //player knocks, so give him stuff
         if (Input.GetButtonDown("Fire1"))
         {
             Vector3 origin = playerCamera.transform.position;
             Vector3 direction = playerCamera.transform.forward;
             RaycastHit hit;
 
-            if (Physics.Raycast(origin, direction, out hit, objectDistance))
+            if (Physics.Raycast(origin, direction, out hit, objectDistance) && knockTimer<=0)
             {
                 if (hit.transform.tag == "Door")
                 {
+                    knockTimer = knockCooldown;
                     Interact();
                 }
             }
         }
+
+        //count down knock timer
+        knockTimer -= Time.deltaTime;
     }
 }
