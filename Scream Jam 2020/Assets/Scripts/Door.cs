e96660d9a18dd5cb4427d72bc15e0e9ee21327b4
vcabrera575 <vcabrera575@gmail.com>
2020-10-13 22:48:13 -0600

    Added an exit Zone Prefab

--- Scream Jam 2020/Assets/Scripts/Door.cs
+++ Scream Jam 2020/Assets/Scripts/Door.cs
@@ -7,7 +7,7 @@ public class Door : MonoBehaviour
 {
     public bool hasBeenKnocked = false;
     public float knockCooldown = 5f; // Time in seconds
-    public float objectDistance = 4f; // Set distance for how far away the player is from interactable object
+    public float objectDistance = 2f; // Set distance for how far away the player is from interactable object
     public float volume = 1f;
 
     public Camera playerCamera;
@@ -18,22 +18,26 @@ public class Door : MonoBehaviour
     // Start is called before the first frame update
     void Interact()
     {
-        Vector3 origin = playerCamera.transform.position;
-        Vector3 direction = playerCamera.transform.forward;
-        RaycastHit hit;
-
-        if (Physics.Raycast(origin, direction, out hit, objectDistance))
-        {
-            hasBeenKnocked = true;
-            doorSoundSource.PlayOneShot(doorSound, volume);
-        }
+        hasBeenKnocked = true;
+        doorSoundSource.PlayOneShot(doorSound, volume);
     }
 
     void Update()
     {
+
         if (Input.GetButtonDown("Fire1"))
         {
-            Interact();
+            Vector3 origin = playerCamera.transform.position;
+            Vector3 direction = playerCamera.transform.forward;
+            RaycastHit hit;
+
+            if (Physics.Raycast(origin, direction, out hit, objectDistance))
+            {
+                if (hit.transform.tag == "Door")
+                {
+                    Interact();
+                }
+            }
         }
     }
 }
---
+++ Scream Jam 2020/Assets/Scripts/exitZone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitZone : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    public float objectDistance = 2f; // Set distance for how far away the player is from interactable object
+
+    // Start is called before the first frame update
+    void Interact()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 origin = playerCamera.transform.position;
+        Vector3 direction = playerCamera.transform.forward;
+        RaycastHit hit;
+
+        if (Input.GetButtonDown("Fire1") && Physics.Raycast(origin, direction, out hit, objectDistance))
+        {
+            // DO something!
+            if (hit.transform.tag == "ExitZone")
+                Debug.Log("Player has clicked on this! exit zone!");
+        }
+    }
+}
