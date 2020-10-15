361b6f0870ad3440371de65d227b692bb41e007c
vcabrera575 <vcabrera575@gmail.com>
2020-10-15 03:35:07 -0600

    Added follower who follows the player around

---
+++ Scream Jam 2020/Assets/Scripts/FollowerScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerScript : MonoBehaviour
+{
+    public GameController gameController;
+    public Transform player;
+
+    public float followSpeed = 2f;
+    public float minDistance = 5f;
+    public float maxDistance = 5f;
+
+    void FollowPlayer()
+    {
+        transform.LookAt(player.position);
+
+        if (Vector3.Distance(transform.position, player.position) >= minDistance)
+        {
+            //transform.position += transform.forward * followSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, player.position) <= maxDistance)
+            {
+                // Player is caught!
+                gameController.Caught();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController.followPlayer)
+            FollowPlayer();
+    }
+
+}
--- Scream Jam 2020/Assets/Scripts/GameController.cs
+++ Scream Jam 2020/Assets/Scripts/GameController.cs
@@ -7,18 +7,24 @@ public class GameController : MonoBehaviour
 {
     public int candy = 0;
     public float gameTimer = 100f;
+    public float followerWaitTime = 15f;
     public string message = "";
+
+    float gameStartTime = 0f;
 
     // Game states
     bool inProgress = true;
     bool gameEnded = false;
     bool timerEnded = false;
+    public bool followPlayer = false;
 
     void Start()
     {
+        gameStartTime = gameTimer;
         inProgress = true;
         gameEnded = false;
         timerEnded = false;
+        followPlayer = false;
     }
 
     // Update is called once per frame
@@ -26,6 +32,10 @@ public class GameController : MonoBehaviour
     {
         //count down timer
         gameTimer -= Time.deltaTime;
+
+        if (gameTimer <= gameStartTime - followerWaitTime)
+            followPlayer = true;
+
     }
 
     //get candies
@@ -38,5 +48,10 @@ public class GameController : MonoBehaviour
     public void SetMessage(string newMessage)
     {
         message = newMessage;
+    }
+
+    public void Caught()
+    {
+        gameEnded = true;
     }
 }
