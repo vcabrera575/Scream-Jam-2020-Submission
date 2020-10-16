1fc8c65f60a6150e86e3529adcfc2c0fa66c85f0
Famichad <72538165+Famichad@users.noreply.github.com>
2020-10-16 01:54:44 -0700

    added menu and enemy pathfinding

--- Scream Jam 2020/Assets/Scripts/FollowerScript.cs
+++ Scream Jam 2020/Assets/Scripts/FollowerScript.cs
@@ -1,37 +1,76 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowerScript : MonoBehaviour
 {
     public GameController gameController;
+    public NavMeshAgent agent;
     public Transform player;
+    Vector3 playerPos;
 
     public float minDistance = 5f;
     public float maxDistance = 5f;
 
+    enum Chase { Chasing, Wandering};
+    Chase chaseState = Chase.Chasing; //0 = chasing player, 1 = wandering
+
+    private void Start()
+    {
+        playerPos = player.position;
+    }
     void FollowPlayer()
     {
-        transform.LookAt(player.position);
 
-        if (Vector3.Distance(transform.position, player.position) >= minDistance)
+        transform.LookAt(player.position);
+        agent.speed = gameController.followerSpeed;
+
+        Vector3 dir = player.position - transform.position;
+        RaycastHit hit;
+        //near position
+        if (Vector3.Distance(transform.position, playerPos) <= maxDistance)
         {
-            // Move the enemy towards the player.
-            transform.position = Vector3.MoveTowards(transform.position, player.position, (gameController.followerSpeed * Time.deltaTime) );
-            if (Vector3.Distance(transform.position, player.position) <= maxDistance)
+            // Player is caught!
+            gameController.Caught();
+            //player isn't in sight so wander
+            if (Physics.Raycast(transform.position, dir, out hit))
             {
-                // Player is caught!
-                gameController.Caught();
+                if (hit.transform.tag != "Player")
+                {
+                    chaseState = Chase.Wandering;
+                }
             }
         }
+        //update player position if in line of sight
+        if (Physics.Raycast(transform.position, dir, out hit))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                playerPos = player.position;
+            }
+            
+        }
+        //actually move
+        agent.SetDestination(playerPos);
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (gameController.followPlayer)
+        //chasing player
+        if (chaseState == Chase.Chasing && gameController.followPlayer)
             FollowPlayer();
+
+        //found player so start chasing
+        Vector3 dir = player.position - transform.position;
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, dir, Color.white, 0.1f, false);
+        if (Physics.Raycast(transform.position, dir, out hit))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                chaseState = Chase.Chasing;
+            }
+
+        }
     }
 
 }
