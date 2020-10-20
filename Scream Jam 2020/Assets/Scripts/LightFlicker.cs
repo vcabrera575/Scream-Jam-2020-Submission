d9083ebe822e9e10c53291f5e6ae846232b00b8b
vcabrera575 <vcabrera575@gmail.com>
2020-10-20 03:37:48 -0600

    Added currently unused light flicker script. Rotated hand to look a little more natural

---
+++ Scream Jam 2020/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public bool flicker = false;
+    public new Light light;
+    public Light halo;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light == null)
+            return;
+
+        StartCoroutine(FlickerLight());
+    }
+
+    IEnumerator FlickerLight()
+    {
+        float flickerSpeed = 5f;
+        int randomizer = 0;
+        while (true)
+        {
+            if (randomizer >= 99)
+            {
+                light.enabled = false;
+            }
+            else
+                light.enabled = true;
+            randomizer = Random.Range(0, 100);
+            yield return new WaitForSeconds(flickerSpeed);
+        }
+    }
+}
