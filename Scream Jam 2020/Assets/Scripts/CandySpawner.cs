b55e4e4ea24c61bd7920fc6c8bcf938ec788b79b
Famichad <72538165+Famichad@users.noreply.github.com>
2020-10-22 19:36:30 -0700

    win screen now has little candy graphic

---
+++ Scream Jam 2020/Assets/Scripts/CandySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySpawner : MonoBehaviour
+{
+    public Rigidbody candy;
+    int candyScore;
+    void Start()
+    {
+        candyScore = PlayerPrefs.GetInt("score", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (candyScore > 0)
+        {
+            Rigidbody c = Instantiate(candy, transform.position, transform.rotation);
+            candyScore--;
+        }
+    }
+}
