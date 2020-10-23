9b302e779d3525cdf1c8b2d8a635c3e810b92b0a
vcabrera575 <vcabrera575@gmail.com>
2020-10-22 20:59:41 -0600

    Added main menu with rotating camera

--- Scream Jam 2020/Assets/Scripts/MainMenu.cs
+++ Scream Jam 2020/Assets/Scripts/MainMenu.cs
@@ -2,6 +2,13 @@
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -11,4 +18,25 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+
+    }
+
+    public void ChangeMusic()
+    {
+
+    }
+
+    void Update()
+    {
+        mainCamera.transform.Rotate(0, 3f * Time.deltaTime, 0);
+    }
+
 }
